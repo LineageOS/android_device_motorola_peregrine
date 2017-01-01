d4e02c557be30fe321d52d65020041a4e3b1f3d9
LuK1337 <priv.luk@gmail.com>
2017-01-01 23:08:59 +0100

    peregrine: cm -> lineage
    Change-Id: Iea2f95c3c9477d63cdcdadccf408753677b6ab86

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
-PRODUCT_NAME := cm_peregrine
+PRODUCT_NAME := lineage_peregrine
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
